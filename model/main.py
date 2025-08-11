58ff998d2da1f7d400dba1d4cfdf0d07f0836d90
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2025-08-12 02:14:25 +0530

    Added extra routes

--- model/main.py
+++ model/main.py
@@ -3,7 +3,7 @@ import logging
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
 from pathlib import Path
-
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 env_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=env_path) 
@@ -17,6 +17,12 @@ origins = [
     "http://localhost:3000",  # React dev server
     "https://academic-q-a-bot.vercel.app/"  # Add production frontend URL here
 ]
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.5,
+    max_tokens=600,
+    )
 
 app = FastAPI(title="LangChain API", version="1.0")
 app.add_middleware(
@@ -49,4 +55,13 @@ async def ask_question(request: QueryRequest):
 
 @app.post("/test")
 async def ask_question(request: QueryRequest):
+    key = os.getenv("OPENAI_API_KEY")
+    print(key)
     return {"question": request.question}
+
+
+@app.post("/test1")
+async def ask_question(request: QueryRequest):
+    print("Started")
+    answer=llm.invoke(request.question)
+    return {"question": request.question,"answer": answer}
